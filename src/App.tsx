e957cae49d0d6b6a76b0935dab2314fc04619276
marcus-piccoli <marcus.piccoli@gmail.com>
2024-02-27 21:17:05 -0300

    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
+// Componente estilizado: inclusive pode-se estilizar uma tag dentro da outra
+const Title = styled.h1`
+  font-size: 2rem;
+  color: blue;
+`;
+
+// Componente funcional do React
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Title>Styled Components</Title>;
 }
 
 export default App;
